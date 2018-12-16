c375d754b2d364698d1d73d2a4c120406777b502
ObieWalker <obinnawalker@gmail.com>
2018-12-16 15:17:16 +0100

    add page functionality - add reset functionality - handle dynamic steps - show completed steps

--- src/components/presentational/CompletedStep.jsx
+++ src/components/presentational/CompletedStep.jsx
@@ -4,10 +4,12 @@ import '../../styles/App.css'
 
 const CompletedStep = (props) => {
 
+  const completed = props.currentStep >= props.stepNumber ? 'filled-step': ''
+
   return (
     <>
-    <div className="progress-bar"></div>
-    <div className="completed-step">
+    <div className={`progress-bar ${completed}`}></div>
+    <div className={`completed-step ${completed}`}>
     <p style={{ fontSize: '15px', marginTop: '40px', textAlign: 'center'}}>{props.stepNumber ? `Step ${props.stepNumber}`: 'Start'}</p>
     </div>
     </>
